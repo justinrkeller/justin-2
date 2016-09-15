965c4a93055279c500a5dcae7a8b7ed60be4e195
Justin Keller <justinrkeller@ucla.edu>
2016-09-14 19:08:36 -0700

    Plot_heights_with_script

--- Exercise-1-control-flow.R
+++ Exercise-1-control-flow.R
@@ -108,3 +108,25 @@ Fun<-function(x) {
 }
 
 #Answer for exercise 9
+#####################################
+#Plotting assignment begins below 
+
+get_heights=function(x){
+  heights<-rnorm(x,69,10)
+  return(mean(heights))
+}
+mean_heights_100<-rep(0,1000)
+for(i in 1:1000){
+  mean_heights_100[i]<-get_heights(100)
+}
+mean_heights_1000<-rep(0,1000)
+for(i in 1:1000){
+  mean_heights_1000[i]<-get_heights(1000)
+}
+bins<-seq(65,73,by=1)
+counts_100<-hist(mean_heights_100,breaks=bins)$counts
+counts_1000<-hist(mean_heights_1000,breaks=bins)$counts
+barplot(rbind(counts_100,counts_1000), col=c(2,4), beside=T,names.arg=seq(65,72,by=1), xlab="Average height(in)", ylab="Count")
+legend('topright',c('n=100','n=100'), col=c(2,4),lwd=4)
+
+
